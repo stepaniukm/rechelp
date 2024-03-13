8f839f754960962d406d185ac2d643af92fdae2e
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2024-03-13 20:50:11 +0100

    chore: remove unnecessary comments

--- app/auth/handlers/google.ts
+++ app/auth/handlers/google.ts
@@ -13,43 +13,11 @@ import { useRuntimeConfig } from "#imports";
 import type { OAuthConfig } from "#auth-utils";
 
 export interface OAuthGoogleConfig {
-  /**
-   * Google OAuth Client ID
-   * @default process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID
-   */
   clientId?: string;
-
-  /**
-   * Google OAuth Client Secret
-   * @default process.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET
-   */
   clientSecret?: string;
-
-  /**
-   * Google OAuth Scope
-   * @default []
-   * @see https://developers.google.com/identity/protocols/oauth2/scopes#google-sign-in
-   * @example ['email', 'openid', 'profile']
-   */
   scope?: string[];
-
-  /**
-   * Google OAuth Authorization URL
-   * @default 'https://accounts.google.com/o/oauth2/v2/auth'
-   */
   authorizationURL?: string;
-
-  /**
-   * Google OAuth Token URL
-   * @default 'https://oauth2.googleapis.com/token'
-   */
   tokenURL?: string;
-
-  /**
-   * Extra authorization parameters to provide to the authorization URL
-   * @see https://developers.google.com/identity/protocols/oauth2/web-server#httprest_3
-   * @example { access_type: 'offline' }
-   */
   authorizationParams?: Record<string, string>;
 }
 
